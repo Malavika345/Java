3ae6176b0836aee379f33774cca310b2112ae001
Malavika <65840561+Malavika345@users.noreply.github.com>
2021-06-12 16:40:01 +0530

    2 programs added

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] arg) {
+        int a, res = 0, n;
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        while (n != 0) {
+            a = n % 10;
+            res = (res * 10) + a;
+            n = n / 10;
+        }
+        System.out.println("reverse of a number is " + res);
+    }
+}
