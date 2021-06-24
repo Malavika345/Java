990421ccbe38cfdbe25e607c51691d4605ed98a7
Malavika <65840561+Malavika345@users.noreply.github.com>
2021-06-24 09:45:37 +0530

    Update Recursionfact.java

--- Recursionfact.java
+++ Recursionfact.java
@@ -6,13 +6,13 @@ public class Recursionfact {
         int num, factorial;
         System.out.println("Enter a number");
         num = new Scanner(System.in).nextInt();
-        factorial = multiplyNumbers(num);
+        factorial = fact(num);
         System.out.println("Factorial of " + num + " = " + factorial);
     }
 
-    public static int multiplyNumbers(int num) {
+    public static int fact(int num) {
         if (num >= 1)
-            return num * multiplyNumbers(num - 1);
+            return num * fact(num - 1);
         else
             return 1;
     }
